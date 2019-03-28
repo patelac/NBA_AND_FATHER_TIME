88bbabd3ce2b83d04429567260c63c4d7e1bdc6a
Akash Patel <patelac@g.cofc.edu>
2019-03-28 10:04:20 -0400

    Some edits before saving

--- scripts/code.R
+++ scripts/code.R
@@ -5,6 +5,12 @@ player_data$career_len = player_data$year_end - player_data$year_start
 
 player_sub = subset(player_data, career_len >= 5 & career_len <= 15)
 
+Seasons_Stats = read.csv('./data/Seasons_Stats.csv')
+***Seasons_sub = subset(Seasons_Stats, Player)
+
+***common <- intersect(player_sub$name, Seasons_Stats$Player)  
+player_sub$name[common,] # give you common rows in data frame 1  
+Seasons_Stats$Player[common,] # give you common rows in data frame 2
 # initial exploration ------
 
 summary(player_data)
@@ -13,8 +19,8 @@ plot(FGP, Age)
 plot(Two_PFGP, Age)
 plot(Three_PFGP, Age)
 
-}
 
+## Vector creations to see set list of names and careers aside from the view in player_sub
 CareerVector <- ((player_data$year_end - player_data$year_start) + 1)
 Names <- (player_data$name)
 CFrame <- data.frame(CareerVector, Names)
@@ -30,14 +36,12 @@ subset(CFrame, CareerVector >= 5)
 CFrame3 <- subset(CFrame, CareerVector <= 15)
 View(CFrame3)
 
-```
-
-```{r Aquired list of valid names for tests}
+## Aquired list of valid names for tests
 
 CFrame4 <- subset(CFrame3, CareerVector >= 5 )
 View(CFrame4)
 
 FinalNames <- CFrame4$Names
 
-```
+
 
