9a754ec4be26217bd6feae1334d6327fd25a1873
Akash Patel <patelac@g.cofc.edu>
2019-04-04 10:19:17 -0400

    updated analysis to lm instead of aov

--- scripts/code.R
+++ scripts/code.R
@@ -44,57 +44,37 @@ View(CFrame4)
 
 FinalNames <- CFrame4$Names
 
-## Fit models with appropriate data with only repeats that have a career length of 5-15 years
+## Fit models and data analysis with appropriate data with only repeats that have a career length of 5-15 years
 
-ggplot(Seasons_sub, aes(x= Seasons_sub$Age , y= Seasons_sub$`FG%`)) + geom_point(size=1, shape=23) + geom_smooth(method=lm)
-ggplot(Seasons_sub, aes(x= Seasons_sub$Age , y= Seasons_sub$`FT%`)) + geom_point(size=1, shape=1) + geom_smooth(method=lm)
-ggplot(Seasons_sub, aes(x= Seasons_sub$Age , y= Seasons_sub$AST)) + geom_point(size=1, shape=2) + geom_smooth(method=lm)
-ggplot(Seasons_sub, aes(x= Seasons_sub$Age , y= Seasons_sub$`TS%`)) + geom_point(size=1, shape=3) + geom_smooth(method=lm)
-ggplot(Seasons_sub, aes(x= Seasons_sub$Age , y= Seasons_sub$PTS)) + geom_point(size=1, shape=20) + geom_smooth(method=lm)
-ggplot(Seasons_sub, aes(x= Seasons_sub$Age , y= Seasons_sub$G)) + geom_point(size=1, shape=22) + geom_smooth(method=lm)
+ggplot(Seasons_sub, aes(x= Seasons_sub$Age , y= Seasons_sub$`FG%`)) +
+  geom_point(size=1, shape=23) +
+  geom_smooth(method=lm) +
+  labs(xlab="Age", ylab="FG%")
+ggplot(Seasons_sub, aes(x= Seasons_sub$Age , y= Seasons_sub$`FT%`)) +
+  geom_point(size=1, shape=1) +
+  geom_smooth(method=lm) + (xlab="Age", ylab="FT%")
+ggplot(Seasons_sub, aes(x= Seasons_sub$Age , y= Seasons_sub$AST)) + geom_point(size=1, shape=2) + geom_smooth(method=lm) + (xlab="Age", ylab="AST")
+ggplot(Seasons_sub, aes(x= Seasons_sub$Age , y= Seasons_sub$`TS%`)) + geom_point(size=1, shape=3) + geom_smooth(method=lm) + (xlab="Age", ylab="TS%")
+ggplot(Seasons_sub, aes(x= Seasons_sub$Age , y= Seasons_sub$PTS)) + geom_point(size=1, shape=20) + geom_smooth(method=lm) + (xlab="Age", ylab="PTS")
+ggplot(Seasons_sub, aes(x= Seasons_sub$Age , y= Seasons_sub$G)) + geom_point(size=1, shape=22) + geom_smooth(method=lm) + (xlab="Age", ylab="Games")
 
 
 FGP_log <- log(Seasons_sub$`FG%`)
 ggplot(Seasons_sub, aes(x= Seasons_sub$Age , y= FGP_log)) + geom_point(shape=1) + geom_smooth(method=lm)
 
-hist(Seasons_sub$`FG%`) 
-hist(Seasons_sub$`TS%`)
+hist(Seasons_sub$`FG%`, xlab="FG%", col = "red") 
+hist(Seasons_sub$`TS%`, xlab ="TS%", col = "blue")
 
-boxplot(Seasons_sub$`FG%` ~ Seasons_sub$Age,
-        +       data = Seasons_sub,
-        +         ylab="FG%",
-        +         xlab="Age")
+boxplot(Seasons_sub$`FG%` ~ Seasons_sub$Age, data = Seasons_sub, ylab="FG%", xlab="Age")
+boxplot(Seasons_sub$`FT%` ~ Seasons_sub$Age, data = Seasons_sub, ylab="FT%", xlab="Age")
+boxplot(Seasons_sub$`TS%` ~ Seasons_sub$Age, data = Seasons_sub, ylab="TS%", xlab="Age")
+boxplot(Seasons_sub$PTS ~ Seasons_sub$Age, data = Seasons_sub, ylab="PTS", xlab="Age")
+boxplot(Seasons_sub$G ~ Seasons_sub$Age, data = Seasons_sub, ylab="Games", xlab="Age")
+boxplot(Seasons_sub$AST ~ Seasons_sub$Age, data = Seasons_sub, ylab="AST", xlab="Age")
 
-boxplot(Seasons_sub$`FT%` ~ Seasons_sub$Age,
-        +         data = Seasons_sub,
-        +         ylab="FT%",
-        +         xlab="Age")
-
-boxplot(Seasons_sub$`TS%` ~ Seasons_sub$Age,
-        +         data = Seasons_sub,
-        +         ylab="TS%",
-        +         xlab="Age")
-
-boxplot(Seasons_sub$PTS ~ Seasons_sub$Age,
-        +         data = Seasons_sub,
-        +         ylab="PTS",
-        +         xlab="Age")
-
-boxplot(Seasons_sub$G ~ Seasons_sub$Age,
-        +         data = Seasons_sub,
-        +         ylab="Games",
-        +         xlab="Age")
-
-boxplot(Seasons_sub$AST ~ Seasons_sub$Age,
-        +         data = Seasons_sub,
-        +         ylab="AST",
-        +         xlab="Age")
-
-FGvsAGE.aov <- aov(Seasons_sub$`FG%` ~ Seasons_sub$Age, data = Seasons_sub)
-summary(FGvsAGE.aov)
-
-FTvsAGE.aov <- aov(Seasons_sub$`FT%` ~ Seasons_sub$Age, data = Seasons_sub)
-summary(FTvsAGE.aov)
-
-TSvsAGE.aov <- aov(Seasons_sub$`TS%` ~ Seasons_sub$Age, data = Seasons_sub)
-summary(TSvsAGE.aov)
+FGvsAGE.lm <- lm(Seasons_sub$`FG%` ~ Seasons_sub$Age, data = Seasons_sub)
+summary(FGvsAGE.lm)
+FTvsAGE.lm <- lm(Seasons_sub$`FT%` ~ Seasons_sub$Age, data = Seasons_sub)
+summary(FTvsAGE.lm)
+TSvsAGE.lm <- lm(Seasons_sub$`TS%` ~ Seasons_sub$Age, data = Seasons_sub)
+summary(TSvsAGE.lm)
